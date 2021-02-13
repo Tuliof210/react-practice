f7ccc7350575c2d976b969618d41c91895e09cd8
Tulio Ferreira <tuliof210@gmail.com>
2021-02-12 23:34:31 -0300

    first component

--- src/index.jsx
+++ src/index.jsx
@@ -1,16 +1,15 @@
 // Ambos imports sao sempre necessários em JSX
 import React from 'react';
 import ReactDOM from 'react-dom';
-
 //styles
 import './index.scss';
+//components
+import First from './App/firstApp/first';
 
 // Não é JS e sim JSX
 const content = (
   <div className="teste">
-    <div className="item"></div>
-    <div className="item"></div>
-    <div className="item"></div>
+    <First></First>
   </div>
 );
 
