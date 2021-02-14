50ba583a238560aea0c0a5efa283b9e4d9d2327b
Tulio Ferreira <tuliof210@gmail.com>
2021-02-14 00:52:29 -0300

    using constructor

--- src/App/Class-App/ClassApp.jsx
+++ src/App/Class-App/ClassApp.jsx
@@ -11,47 +11,26 @@ export default class Saudacao extends Component {
   // criação do estado inicial
   // cada instancia tem seu proprio estado
   interval;
-  state = {
-    tipo: this.props['tipo'],
-    nome: this.props['nome'],
-    btnTXT: 'COMEÇAR',
-    count: 0,
-    counting: false,
-  };
-  render() {
-    // Forma de acessar props em component de classe
-    // Usa-se a palavra 'this' para referenciar a instancia atual
-    const { count, btnTXT, tipo, nome } = this.state;
-    return (
-      <div className="classAppWrapper">
-        <h1>Timer: {count}</h1>
-        <hr />
-        <input
-          type="text"
-          placeholder="Tipo..."
-          value={tipo}
-          onChange={(e) => this.setTipo(e)}
-        />
-        <input
-          type="text"
-          placeholder="Nome..."
-          value={nome}
-          onChange={(e) => this.setTipo(e)}
-        />
-        <button className="default-btn" onClick={(e) => this.startCount()}>
-          {btnTXT}
-        </button>
-      </div>
-    );
+
+  // necessario usar 'super' em contructor ja que a classe em questao
+  // deriva da classe component
+  constructor(props) {
+    super(props);
+    this.state = {
+      tipo: this.props['tipo'],
+      nome: this.props['nome'],
+      btnTXT: 'COMEÇAR',
+      count: 0,
+      counting: false,
+    };
   }
-  setTipo(event) {
+
+  callSetState({ event, ref }) {
     // use isso para alterar o state
     // recebe um objeto com 'n' atributos que desejamos alterar
     this.setState({
-      tipo: event['target']['value'],
+      [ref]: event['target']['value'],
     });
-
-    console.log({ val: event['target']['value'] });
   }
 
   startCount() {
@@ -75,4 +54,30 @@ export default class Saudacao extends Component {
 
   o DADO que manda na atualização do component
   */
+  render() {
+    // Forma de acessar props em component de classe
+    // Usa-se a palavra 'this' para referenciar a instancia atual
+    const { count, btnTXT, tipo, nome } = this.state;
+    return (
+      <div className="classAppWrapper">
+        <h1>Timer: {count}</h1>
+        <hr />
+        <input
+          type="text"
+          placeholder="Tipo..."
+          value={tipo}
+          onChange={(event) => this.callSetState({ event, ref: 'tipo' })}
+        />
+        <input
+          type="text"
+          placeholder="Nome..."
+          value={nome}
+          onChange={(event) => this.callSetState({ event, ref: 'nome' })}
+        />
+        <button className="default-btn" onClick={(e) => this.startCount()}>
+          {btnTXT}
+        </button>
+      </div>
+    );
+  }
 }
